bd022ff69c90ffe3df3475fa16ed7885899139f3
Eric Walter PEFURA YONE <88545079+pefura@users.noreply.github.com>
2025-12-11 19:33:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import joblib
 
+import sklearn
+import numpy
 st.error(f"DEBUG INFO: Scikit-learn Version: {sklearn.__version__}")
 st.error(f"DEBUG INFO: Numpy Version: {numpy.__version__}")
 
